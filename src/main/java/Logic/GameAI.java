b6a4ccb37b8b497c6d8fae5da6888d363fd0a9c3
nikita280799 <nikita280799@mail.ru>
2020-10-04 16:01:48 +0900

    add comments

--- src/main/java/Logic/GameAI.java
+++ src/main/java/Logic/GameAI.java
@@ -35,31 +35,40 @@ public class GameAI {
         while (!candidates.isEmpty()) {
             heuristic++;
             Set<Cell> newCandidates = new HashSet<>();
+            //получаем мн-во возможных ходов данной шашки и если еще не были там добавляем клетку в мн-во новых кандидатов
             candidates.forEach(cell -> board.getAvaliableSteps(cell, true).forEach(avaliableStep -> {
                 if (!visitedCells.contains(avaliableStep)) newCandidates.add(avaliableStep);
             }));
             for (Cell cell : newCandidates) {
+                //если одна из клеток кандидатов победная для овцы возвращаем кол-во ходов до нее
                  if (cell.isItSheepWinCell()) return heuristic;
             }
+            //записываем все обработанные клетки в мн-во посещанных
             visitedCells.addAll(newCandidates);
             candidates = new HashSet<>(newCandidates);
         }
         return 100 - visitedCells.size();
     }
 
-    public int runMinMax(boolean isItSheep, int recursiveLevel, int alpha, int beta) {
+    /** Алгоритм минимакса с альфа-бета отсечением и глубиной просчета зависящей от level
+     */
+    public int runMinMax(boolean isItSheep, int recursionLevel, int alpha, int beta) {
         int curHeuristic;
         Cell bestStep = new Cell(0, 0);
         Cell bestStepFor = new Cell(0, 0);
-        if (recursiveLevel >= level) {
+        if (recursionLevel >= level) {
             return getHeuristic();
         }
         int bestHeuristic = isItSheep ? 100 : 0;
+        //для каждого возможного хода запускаем минимакс с целью получения эвристики
+        //при этом лучшую эвристику запоминаем и совершаем соответсвующий ход
+
+        //если ход овцы
         if (isItSheep) {
             for (Cell newCell : board.getAvaliableSteps(board.sheepCell, true)) {
                 Cell oldCell = board.sheepCell;
                 board.step(oldCell, newCell);
-                curHeuristic = runMinMax(!isItSheep, recursiveLevel + 1, alpha, beta);
+                curHeuristic = runMinMax(!isItSheep, recursionLevel + 1, alpha, beta);
                 if (curHeuristic <= bestHeuristic) {
                     if (curHeuristic < bestHeuristic || newCell.i > bestStep.i) {
                         bestHeuristic = curHeuristic;
@@ -69,14 +78,20 @@ public class GameAI {
                 }
                 board.step(newCell, oldCell);
                 beta = Math.min(curHeuristic, beta);
-                if (beta < alpha) break;
+                //alpha — текущее максимальное значение меньше которого волки никогда не выберут,
+                //а beta — текущее минимальное значение больше которого овца никогда не выберет
+                //если alpha > beta что означает конфликт ожиданий, эту ветвь ходов можно не рассматривать
+                //а потому прерываем цикл
+                if (alpha > beta) break;
             }
-        } else {
+        }
+        //если ход волков
+        else {
             Set<Cell> wolfsPositionsCopy = new HashSet<>(board.wolfsCells);
             for (Cell oldCell : wolfsPositionsCopy) {
                 for (Cell newCell : board.getAvaliableSteps(oldCell, false)) {
                     board.step(oldCell, newCell);
-                    curHeuristic = runMinMax(!isItSheep, recursiveLevel + 1, alpha, beta);
+                    curHeuristic = runMinMax(!isItSheep, recursionLevel + 1, alpha, beta);
                     if (curHeuristic > bestHeuristic) {
                         bestHeuristic = curHeuristic;
                         bestStep = newCell;
@@ -84,11 +99,12 @@ public class GameAI {
                     }
                     board.step(newCell, oldCell);
                     alpha = Math.max(curHeuristic, alpha);
-                    if (beta < alpha) break;
+                    if (alpha > beta) break;
                 }
             }
         }
-        if (recursiveLevel == 0) {
+        //совершаем лучший найденный ход
+        if (recursionLevel == 0) {
             board.step(bestStepFor, bestStep);
         }
         return bestHeuristic;
